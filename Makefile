c93c0ad654de497a14775c19a26cf04db49f459e
Hugo <hugo.arenas@usach.cl>
2020-08-13 19:01:44 -0400

    Corrección completada Lab 1

--- Makefile
+++ Makefile
@@ -1,8 +1,3 @@
-padre: main.c lectura.c conversion.c filtracion.c binarizacion.c clasificacion.c -ljpeg
-	gcc -Wall main.c -o pipeline -I.
-	gcc -Wall lectura.c -o lectura -ljpeg -I.
-	gcc -Wall conversion.c -o conversion -I.
-	gcc -Wall filtracion.c -o filtracion -I.
-	gcc -Wall binarizacion.c -o binarizacion -I.
-	gcc -Wall clasificacion.c -o clasificacion -ljpeg -I.
+padre: main.c -ljpeg
+	gcc -Wall main.c -o pipeline -ljpeg -I.
 
--- main.c
+++ main.c
@@ -19,6 +19,29 @@
 
 matrixF *convertFilter(char **datefilter, int cont);
 
+matrixF *grayScale(int **pixels, int height, int width);
+
+matrixF *escalaGris(matrixF *mf);
+
+matrixF *filtracion(matrixF *mf, matrixF *filter);
+
+matrixF *binarizacion(matrixF *mf, int umbral);
+
+GLOBAL(void) escribirJPG(char *nombre, matrixF *mf, int fil, int col);
+void clasificacion(matrixF *mf, int umbral, char *namefile, int aux);
+
+METHODDEF(void) my_error_exit (j_common_ptr cinfo);
+
+GLOBAL(matrixF*) leerJPG(char *nombre);
+
+/*Estructura para manejar imágenes tipo .jpg*/
+struct my_error_mgr {
+  struct jpeg_error_mgr pub;	
+  jmp_buf setjmp_buffer;
+};
+typedef struct my_error_mgr * my_error_ptr;
+
+
 // Funcion main: Funcion que toma por parametros los datos entrantes y pasa a la etapa de lectura,
 // La matriz del filtro para convulocion y el nombre de las imagenes.
 // Entrada: los parametros ingresados por el usuario.
@@ -113,91 +136,23 @@ int main(int argc, char *argv[])
 	{
 		printf("\n|     Imagen     |     Nearly Black     |\n");
 	}
-  	
+  	matrixF *filter = convertFilter(datefilter, cont); 
   	while(image <= cantidadImagenes){ /*Se ejecuta while miestras sea cantidadImagenes>0*/
-		matrixF *filter = convertFilter(datefilter, cont); 
-		int status;
-		int pDateMatrix[2];
-		int pFilMatrix[2];
-		int pColMatrix[2];
-		int pUmbral[2];
-		int pUmbralB[2];
-		int pNombre[2];
-		int resultPantalla[2];
 	    char cantidadImg[10];
 	    sprintf(cantidadImg,"%d",image); 
 	    char imagenArchivo[] = "imagen_"; /*Archivo de entrada imagenes*/
 	    char extension[] = ".jpg"; /*Extension de imagen*/
 	    strcat(imagenArchivo,cantidadImg); /*imagen_1*/
 	    strcat(imagenArchivo,extension); /*imagen_1.jpg*/
-	    
-	    /*Se crean los pipes*/
-	    pipe(pNombre); /*Para pasar el nombreImagen.*/
-	    pipe(pUmbral); /*Para pasar el umbral para clasificacion.*/
-		pipe(pUmbralB);
-	    pipe(pDateMatrix);
-		pipe(pFilMatrix);
-		pipe(pColMatrix);
-		pipe(resultPantalla);
-	    /*Se crea el proceso hijo*/
-
-	    pid_t pid = fork();
-	    /*Mayor que 0 es el PADRE*/
-	    if(pid>0){
-
-	    	close(pNombre[0]); /*Se cierra la lectura*/
-	      	write(pNombre[1],imagenArchivo,(strlen(imagenArchivo)+1));
-	      	close(pUmbral[0]); /*Se cierra la lectura*/
-	      	write(pUmbral[1],umbralClasificacion,sizeof(umbralClasificacion));
-			close(pUmbralB[0]); /*Se cierra la lectura*/
-	      	write(pUmbralB[1],umbralBinarizacion,sizeof(umbralBinarizacion));
-	      	close(resultPantalla[0]);
-	      	write(resultPantalla[1],aux,sizeof(aux));
-			close(pDateMatrix[0]);
-			close(pFilMatrix[0]);
-			close(pColMatrix[0]);
-			int filmatrix = countFil(filter);
-			int colmatrix = countColumn(filter);
-			write(pFilMatrix[1], &filmatrix, sizeof(filmatrix));
-			write(pColMatrix[1], &colmatrix, sizeof(colmatrix));
-			for (int y = 0; y < countFil(filter); y++){
-				for (int x = 0; x < countColumn(filter); x++){
-					float datematrix = getDateMF(filter, y, x);
-					write(pDateMatrix[1], &datematrix, sizeof(datematrix));
-				}
-			}
-	      	waitpid(pid,&status,0);
-
-	    }else{/*Es hijo*/
-
-	      	close(pNombre[1]);
-	      	dup2(pNombre[0],3);
-
-	      	close(pUmbral[1]);
-	      	dup2(pUmbral[0],4);
-
-			close(pUmbralB[1]);
-	      	dup2(pUmbralB[0],5);
-
-			close(resultPantalla[1]);
-			dup2(resultPantalla[0],6);
-
-			close(pDateMatrix[1]);
-	      	dup2(pDateMatrix[0], 7);
-
-			close(pFilMatrix[1]);
-	      	dup2(pFilMatrix[0], 8);
-
-			close(pColMatrix[1]);
-	      	dup2(pColMatrix[0], 9);
-
-	      	char *argvHijo[] = {"lectura",NULL}; /*Nombre del archivo al cual pasara el hijo*/
-	      	execv(argvHijo[0],argvHijo); /*Reemplaza el codigo del proceso, por el cual apunta argvHijo*/
-	    }
-	      image++;
+		matrixF *mf = leerJPG(imagenArchivo);
+		mf = escalaGris(mf);
+		mf = filtracion(mf,filter);
+		mf = binarizacion(mf,umbralBinarizacion[0]);
+		char *imagefile = (char *)malloc(1000*sizeof(char));
+		strncpy(imagefile, imagenArchivo, strlen(imagenArchivo)-4);
+		clasificacion(mf,umbralClasificacion[0],imagefile,aux[0]);
+	    image++;
   	}/*Fin while*/
-
-
 }/*Fin Main*/
 
 
@@ -250,3 +205,208 @@ matrixF *convertFilter(char **datefilter, int cont){
 	return filter;
 }
 
+METHODDEF(void)
+my_error_exit (j_common_ptr cinfo){
+	my_error_ptr myerr = (my_error_ptr) cinfo->err;
+	(*cinfo->err->output_message) (cinfo);
+	longjmp(myerr->setjmp_buffer, 1);
+}
+
+/*Función que se encarga de leer imágen en formato .jpg*/
+/*Entrada: Imágen jpg.*/
+/* Salida: Imagen leída, caso contrario retorna cero*/
+GLOBAL(matrixF*)
+leerJPG(char *nombre){
+	struct jpeg_decompress_struct cinfo;
+	struct my_error_mgr jerr;
+	FILE * imagen;
+	JSAMPARRAY buffer;
+	int row_stride;
+	if ((imagen = fopen(nombre, "rb")) == NULL) {
+		fprintf(stderr, "can't open %s\n", nombre);
+		return 0;
+	}
+	cinfo.err = jpeg_std_error(&jerr.pub);
+	jerr.pub.error_exit = my_error_exit;
+	if (setjmp(jerr.setjmp_buffer)) {
+		jpeg_destroy_decompress(&cinfo);
+		fclose(imagen);
+		return 0;
+	}
+	jpeg_create_decompress(&cinfo);
+	jpeg_stdio_src(&cinfo, imagen);
+	(void) jpeg_read_header(&cinfo, TRUE);
+	(void) jpeg_start_decompress(&cinfo);
+	row_stride = cinfo.output_width * cinfo.output_components;
+	unsigned char* filaPixel = (unsigned char*)malloc( cinfo.output_width*cinfo.output_height*cinfo.num_components );
+	buffer = (*cinfo.mem->alloc_sarray)((j_common_ptr) &cinfo, JPOOL_IMAGE, row_stride, 1);
+	int acum = 0;
+	while (cinfo.output_scanline < cinfo.output_height) {
+		(void) jpeg_read_scanlines(&cinfo, buffer, 1);
+		for(int i = 0;i < cinfo.image_width*cinfo.num_components;i++){
+			filaPixel[i + acum] = buffer[0][i];
+			if (i == cinfo.image_width*cinfo.num_components - 1){
+				acum = acum + i + 1;
+			}
+		}
+	}
+	matrixF *mf = createMF(cinfo.image_height, cinfo.image_width*3);
+	for (int i = 0; i < cinfo.image_height; i++){
+		for(int j = 0; j < cinfo.image_width; j++)
+		{
+			mf = setDateMF(mf,i,j*3,(float)filaPixel[(i*cinfo.image_width*3)+(j*3)+0]);
+			mf = setDateMF(mf,i,j*3 + 1,(float)filaPixel[(i*cinfo.image_width*3)+(j*3)+1]);
+			mf = setDateMF(mf,i,j*3 + 2,(float)filaPixel[(i*cinfo.image_width*3)+(j*3)+2]);
+		}
+	}
+	(void) jpeg_finish_decompress(&cinfo);
+	jpeg_destroy_decompress(&cinfo);
+	fclose(imagen);
+	return mf;
+}
+
+/*Función que se encarga de convertir pixeles en escala de grises*/
+/*Entrada: pixeles, alto y largo.*/
+/* Salida: Matriz con escala de grises*/
+matrixF *escalaGris(matrixF *mf) {
+	matrixF *newmf = createMF(countFil(mf), countColumn(mf)/3);
+	for(int y = 0; y < countFil(newmf); y++) {
+		for(int x = 0; x < countColumn(newmf); x++) {
+			float prom = getDateMF(mf,y,x*3)*0.299+getDateMF(mf,y,x*3 + 1)*0.587+getDateMF(mf,y,x*3 + 2)*0.114;
+			newmf = setDateMF(newmf, y, x, prom);
+		}
+	}
+	return newmf;
+}
+
+/*Función que se encarga de realizar la convolución a la imágen e imprimirla si se introdujo -b*/
+/*Entrada: imagen y filtro.*/
+/*Salida: Matriz convolucionada*/
+matrixF *filtracion(matrixF *mf, matrixF *filter){
+	if ((countFil(filter) == countColumn(filter))&&(countFil(filter)%2 == 1)){
+		int increase = 0, initial = countFil(filter);
+		while (initial != 1){
+			initial = initial - 2;
+			increase = increase + 1;
+		}
+		
+		matrixF *newmf = createMF(countFil(mf),countColumn(mf));
+		for (int cont = 0; cont < increase; cont++){
+			mf = amplifyMF(mf);
+		}
+		for (int fil = increase; fil < countFil(mf) - increase; fil++){
+			for (int col = increase; col < countColumn(mf) - increase; col++){
+				float sum = 0.0000;
+				for (int y = 0; y < countFil(filter); y++){
+					for (int x = 0; x < countColumn(filter); x++){
+						float result = getDateMF(filter, y, x)*getDateMF(mf, y + fil - increase, x + col - increase);
+						sum = sum + result;
+					}
+				}
+				newmf = setDateMF(newmf, fil - increase, col - increase, sum);
+				
+			}
+		}
+		for (int cont2 = 0; cont2 < increase; cont2++){
+			mf = decreaseMF(mf);
+		}
+		
+		return newmf;
+	}
+	else{
+		return mf;
+	}
+}
+
+/*Función que se encarga de convertir pixeles en valores binarios (0 o 255)*/
+/*Entrada: matriz y umbral.*/
+/*Salida: Matriz binaria*/
+matrixF *binarizacion(matrixF *mf, int umbral){
+  for (int y = 0; y < countFil(mf); y++){
+    for (int x = 0; x < countColumn(mf); x++){
+      if (getDateMF(mf,y,x) <= umbral){
+        mf = setDateMF(mf, y, x, 0.0000);
+      }
+      else{
+        mf = setDateMF(mf, y, x, 255.0000);
+      }
+    }
+  }
+  
+  return mf;
+}
+
+// Funcion: EScribe un archivo en formato png, resultante
+// Entrada: en nombre del archivo y lamatriz resultante.
+// Salida: void
+GLOBAL(void)
+escribirJPG(char *nombre, matrixF *mf, int fil, int col){
+	struct jpeg_compress_struct cinfo;
+	struct jpeg_error_mgr jerr;
+	FILE * outfile;		/* target file */
+	JSAMPROW row_pointer[1];	/* pointer to JSAMPLE row[s] */
+	int row_stride;		/* physical row width in image buffer */
+	cinfo.err = jpeg_std_error(&jerr);
+	jpeg_create_compress(&cinfo);
+	if ((outfile = fopen(nombre, "wb")) == NULL) {
+		fprintf(stderr, "can't open %s\n", nombre);
+		exit(1);
+	}
+
+	jpeg_stdio_dest(&cinfo, outfile);
+	cinfo.image_width = col; 	/* image width and height, in pixels */
+	cinfo.image_height = fil;
+	cinfo.input_components = 3;		/* # of color components per pixel */
+	cinfo.in_color_space = JCS_RGB; 	/* colorspace of input image */
+	jpeg_set_defaults(&cinfo);
+	jpeg_set_quality(&cinfo, 10, TRUE /* limit to baseline-JPEG values */);
+	jpeg_start_compress(&cinfo, TRUE);
+	row_stride = col * 3;	/* JSAMPLEs per row in image_buffer */
+	JSAMPLE *buffer = (JSAMPLE*)malloc(fil*col*3*sizeof(JSAMPLE));
+	unsigned char* pixel_row = (unsigned char*)(buffer);
+	for (int i = 0; i < cinfo.jpeg_height; i++){
+		for(int j = 0; j < cinfo.jpeg_width; j++)
+		{
+			pixel_row[(i*cinfo.jpeg_width*3)+(j*3)+0]=(unsigned char)((int)getDateMF(mf, i, j));
+			pixel_row[(i*cinfo.jpeg_width*3)+(j*3)+1]=(unsigned char)((int)getDateMF(mf, i, j));
+			pixel_row[(i*cinfo.jpeg_width*3)+(j*3)+2]=(unsigned char)((int)getDateMF(mf, i, j));
+		}
+	}
+	buffer = pixel_row;
+	while (cinfo.next_scanline < cinfo.image_height) {
+		row_pointer[0] = &buffer[cinfo.next_scanline * row_stride];
+		(void) jpeg_write_scanlines(&cinfo, row_pointer, 1);
+	}
+	jpeg_finish_compress(&cinfo);
+	fclose(outfile);
+	jpeg_destroy_compress(&cinfo);
+}
+
+// Funcion: Permite clasificar una imagene de acuerdo a un umbral
+// Entrada: Matriz resultante desde etapa de pooling, umbral ingresado por usuario y el nombre d ela imagen.
+// Salida: void
+void clasificacion(matrixF *mf, int umbral, char *namefile, int aux){
+	int maxBlack = 0;
+	for (int y = 0; y < countFil(mf); y++){
+		for (int x = 0; x < countColumn(mf); x++){
+			if (getDateMF(mf, y, x) == 0.0000){
+				maxBlack = maxBlack + 1;
+			}
+		}
+	}
+	float porcentBlack = (maxBlack * 100.0000)/(countFil(mf) * countColumn(mf));
+
+	if (aux == 1)
+	{
+		
+		if (porcentBlack >= umbral){
+			printf("|   %s   |         yes        |\n",namefile);
+		}
+		if (porcentBlack < umbral){
+			printf("|   %s   |         no         |\n",namefile);
+		}
+
+	}
+	strcat(namefile,"R.jpg");
+	escribirJPG(namefile, mf,countFil(mf),countColumn(mf));
+}
